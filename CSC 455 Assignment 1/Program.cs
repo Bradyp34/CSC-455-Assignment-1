8948c32ce279ec7af8db070149812bfa42de105b
Brady Petterson <Brady.Petterson@gmail.com>
2024-01-22 12:09:33 -0600

    Added defualt to choice if invalid. Fixed the random number to take a maximum as a parameter, and fixed the random generator.

--- CSC 455 Assignment 1/Program.cs
+++ CSC 455 Assignment 1/Program.cs
@@ -8,31 +8,40 @@ namespace CSC_455_Assignment_1 {
 	public class MyFunctions {
 		public static void PrintMenu () {
 			// want to make/print the menu with this function
-			//menu of other options given
+			// menu of other options given
 
 			Console.WriteLine("1: Random number 1-10.\n");
 			Console.WriteLine("2: Today's date.\n");
 			Console.WriteLine("3: List of dinosaurs.\n");
 			Console.WriteLine("4: String class methods.\n");
 		}
-		public static int RandomNum () {
+		public static int RandomNum (int max) {
 			// make random number 1-10 and return it
+			Random rand = new Random();
 
-			return (RandomNum() % 10) + 1;
+			return rand.Next(max) + 1;
+		}
+		public static void PrintDate() {
+			// print today's date in MM-DD-YYYY format
+
+			Console.WriteLine($"{DateTime.Now.ToString("MM-DD-YYYY")}");
 		}
 		public static void DoMenu (int choice) {
 			switch (choice) {
 				case 1: // print the random number
-					Console.WriteLine($"{MyFunctions.RandomNum()}\n");
+					Console.WriteLine($"{RandomNum(10)}\n");
 					break;
 				case 2: // today's date
-
+					PrintDate();
 					break;
 				case 3: // dinosaur list
 
 					break;
 				case 4: // string class
 
+					break;
+				default: // if the choice was not valid
+					Console.WriteLine("Invalid choice.\n");
 					break;
 			}
 		}
