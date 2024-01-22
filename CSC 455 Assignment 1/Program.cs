0ebaec38d70b52c6eb0e0a7625237edadea132ee
Brady Petterson <Brady.Petterson@gmail.com>
2024-01-22 12:30:13 -0600

    Created the Dinos list using user input in a while loop. Added ReadLines after the calls so output is read.

--- CSC 455 Assignment 1/Program.cs
+++ CSC 455 Assignment 1/Program.cs
@@ -5,7 +5,7 @@ using System.Text;
 using System.Threading.Tasks;
 
 namespace CSC_455_Assignment_1 {
-	public class MyFunctions {
+	class MyFunctions {
 		public static void PrintMenu () {
 			// want to make/print the menu with this function
 			// menu of other options given
@@ -15,34 +15,64 @@ namespace CSC_455_Assignment_1 {
 			Console.WriteLine("3: List of dinosaurs.\n");
 			Console.WriteLine("4: String class methods.\n");
 		}
-		public static int RandomNum (int max) {
+		private static int RandomNum (int max) {
 			// make random number 1-10 and return it
+			// mostly for practice and organization purposes
+
 			Random rand = new Random();
 
 			return rand.Next(max) + 1;
 		}
-		public static void PrintDate() {
+		private static void PrintDate () {
 			// print today's date in MM-DD-YYYY format
 			// this is my preferred method of printing the date
 
 			Console.WriteLine($"{DateTime.Now.ToString("MM-DD-YYYY")}");
 		}
+		private static void DinoLister () {
+			Console.WriteLine("Please give me at least 10 Dinosaur names. Write <done> to finish:\n");
+
+			// create list, temporary string, and counter for the user
+			List<string> dinos = new List<string>();
+			int counter = 1;
+			string locum = "";
+
+			// thoroughness for the people -_-
+			while (locum != "done" && locum != "Done" && locum != "<done>" && locum != "<Done>") {
+				Console.WriteLine($"{counter.ToString()}: ");
+				locum = Console.ReadLine();
+				dinos.Add(locum);
+				counter++;
+			}
+
+			dinos.Sort();
+
+			// call the random number method
+			// repurpose the counter int
+			counter = MyFunctions.RandomNum(dinos.Count);
+			Console.WriteLine($"{dinos[counter]}\n");
+		}
 		public static void DoMenu (int choice) {
 			switch (choice) {
 				case 1: // print the random number
 					Console.WriteLine($"{RandomNum(10)}\n");
+					Console.ReadLine();
 					break;
 				case 2: // today's date
 					PrintDate();
+					Console.ReadLine();
 					break;
 				case 3: // dinosaur list
-
+					DinoLister();
+					Console.ReadLine();
 					break;
 				case 4: // string class
 
+					Console.ReadLine();
 					break;
 				default: // if the choice was not valid
 					Console.WriteLine("Invalid choice.\n");
+					Console.ReadLine();
 					break;
 			}
 		}
