0a66f3268d8d198034226bae8a18a26eaa4fedb7
Brady Petterson <Brady.Petterson@gmail.com>
2024-01-22 11:48:42 -0600

    Created the menu and put it in a variable. (Also fixed personal issues with Github.)

---
+++ CSC 455 Assignment 1/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSC_455_Assignment_1 {
+	public class MyFunctions {
+		public static void PrintMenu () {
+			// want to make/print the menu with this function
+			//menu of other options given
+
+			Console.WriteLine("1: Random number 1-10.\n");
+			Console.WriteLine("2: Today's date.\n");
+			Console.WriteLine("3: List of dinosaurs.\n");
+			Console.WriteLine("4: String class methods.\n");
+		}
+		public static int RandomNum () {
+			// make random number 1-10 and return it
+
+			return (RandomNum() % 10) + 1;
+		}
+	}
+	internal class Program {
+		static void Main (string[] args) {
+			MyFunctions.PrintMenu(); // print the menu
+
+			var locum = Console.ReadLine();
+			int choice = int.Parse(locum);
+
+
+			Console.WriteLine($"{MyFunctions.RandomNum()}\n"); // print the random number
+		}
+	}
+}
